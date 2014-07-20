f8ac7c8163c22750f100d68065a01cd38c8c727e
Javier Garcia <javigar88@gmail.com>
2014-07-20 03:22:11 +0200

    Python3 version

---
+++ change.py
@@ -0,0 +1,20 @@
+num = input('Give me a number: ')
+
+try:
+    num_int = int(num)
+except ValueError:
+    print("Invalid number")
+
+def vueltas(cantidad):
+	euros = [500, 200, 100, 50, 20, 10, 5, 2, 1]
+	centimos = [50, 20, 10, 5, 2, 1]
+	  
+	dev_euros = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+	dev_centimos = [0, 0, 0, 0, 0, 0]
+	  
+	cantidad_uso = cantidad;
+
+	for i in range(1, len(euros)):
+		if cantidad_uso >= euros[i]:
+			cantidad_uso = cantidad_uso - euros[i]
+			dev_euros[i] += 1
